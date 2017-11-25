97afbe17695418ecbacae28e2fa23d687d3d7977
Andrey Sitnik <andrey@sitnik.ru>
2017-11-26 02:56:17 +0300

    Add more states to demo

--- test/index.js
+++ test/index.js
@@ -1,8 +1,14 @@
-var recorder, record, stop, events
+var EVENTS = ['start', 'stop', 'pause', 'resume']
+
+var recorder, recordFull, recordParts, pause, resume, stop, list
 
 window.onload = function () {
-  events = document.getElementById('events')
-  record = document.getElementById('record')
+  list = document.getElementById('list')
+
+  recordParts = document.getElementById('recordParts')
+  recordFull = document.getElementById('recordFull')
+  resume = document.getElementById('resume')
+  pause = document.getElementById('pause')
   stop = document.getElementById('stop')
 
   if (typeof MediaRecorder === 'undefined') {
@@ -11,19 +17,30 @@ window.onload = function () {
     return
   }
 
-  record.addEventListener('click', startRecording)
+  recordParts.addEventListener('click', startRecording)
+  recordFull.addEventListener('click', startRecording)
+  resume.addEventListener('click', resumeRecording)
+  pause.addEventListener('click', pauseRecording)
   stop.addEventListener('click', stopRecording)
 
-  record.disabled = false
+  recordParts.disabled = false
+  recordFull.disabled = false
 }
 
-function startRecording () {
+function startRecording (e) {
   navigator.mediaDevices.getUserMedia({ audio: true }).then(function (stream) {
     recorder = new MediaRecorder(stream)
-    recorder.start()
-    record.disabled = true
-    stop.disabled = false
+
+    EVENTS.forEach(function (name) {
+      recorder.addEventListener(name, changeState.bind(null, name))
+    })
     recorder.addEventListener('dataavailable', saveRecord)
+
+    if (e.target === recordFull) {
+      recorder.start()
+    } else {
+      recorder.start(1000)
+    }
   })
 }
 
@@ -32,17 +49,49 @@ function stopRecording () {
   recorder.stream.getTracks().forEach(function (track) {
     track.stop()
   })
-  record.disabled = false
-  stop.disabled = true
+}
+
+function pauseRecording () {
+  recorder.pause()
+}
+
+function resumeRecording () {
+  recorder.resume()
 }
 
 function saveRecord (e) {
-  var li = document.createElement('audio')
+  var li = document.createElement('li')
 
   var audio = document.createElement('audio')
   audio.controls = true
   audio.src = URL.createObjectURL(e.data)
   li.appendChild(audio)
 
-  events.appendChild(li)
+  list.appendChild(li)
+}
+
+function changeState (eventName) {
+  var li = document.createElement('li')
+  li.innerHTML = '<strong>' + eventName + ':</strong> ' + recorder.state
+  list.appendChild(li)
+
+  if (recorder.state === 'recording') {
+    recordParts.disabled = true
+    recordFull.disabled = true
+    resume.disabled = false
+    pause.disabled = false
+    stop.disabled = false
+  } else if (recorder.state === 'paused') {
+    recordParts.disabled = false
+    recordFull.disabled = false
+    resume.disabled = false
+    pause.disabled = true
+    stop.disabled = false
+  } else if (recorder.state === 'inactive') {
+    recordParts.disabled = false
+    recordFull.disabled = false
+    resume.disabled = true
+    pause.disabled = true
+    stop.disabled = true
+  }
 }
