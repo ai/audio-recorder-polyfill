fa4cacbb63dcfee06075d590df5d69cfa4699d5e
Andrey Sitnik <andrey@sitnik.ru>
2017-11-27 04:15:21 +0300

    Compress demo

--- test/demo/webpack.config.js
+++ test/demo/webpack.config.js
@@ -1,4 +1,6 @@
+var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 var path = require('path')
 
 module.exports = {
@@ -12,10 +14,17 @@ module.exports = {
     filename: '[name].js'
   },
   plugins: [
+    new UglifyJSPlugin(),
     new HtmlWebpackPlugin({
+      inlineSource: 'index.js',
       template: path.join(__dirname, 'index.html'),
-      chunks: ['index']
-    })
+      chunks: ['index'],
+      minify: {
+        collapseWhitespace: true,
+        minifyCSS: true
+      }
+    }),
+    new HtmlWebpackInlineSourcePlugin()
   ],
   devServer: {
     contentBase: path.join(__dirname, 'build')
