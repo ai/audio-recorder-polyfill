133262f425aafaee96689059d956eb5b594ba6e9
Andrey Sitnik <andrey@sitnik.ru>
2018-03-08 09:42:22 -0500

    Update dependencies

--- test/demo/webpack.config.js
+++ test/demo/webpack.config.js
@@ -1,9 +1,9 @@
 var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 var path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: path.join(__dirname, 'index.js'),
     polyfill: path.join(__dirname, 'polyfill.js')
@@ -14,7 +14,6 @@ module.exports = {
     filename: '[name].js'
   },
   plugins: [
-    new UglifyJSPlugin(),
     new HtmlWebpackPlugin({
       inlineSource: 'index.js',
       template: path.join(__dirname, 'index.html'),
