2895b63183b045c6aa4c12962db978da07907a7c
SriHarsha Gangisetty <sriharshagangisetty@gmail.com>
2019-01-20 23:50:42 +0530

    fixed cannot start recording when recording stopped due to cloned stream tracks being ended

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function MediaRecorder (stream) {
    * The `MediaStream` passed into the constructor.
    * @type {MediaStream}
    */
-  this.stream = stream.clone()
+  this.stream = stream
 
   /**
    * The current state of recording process.
@@ -78,6 +78,9 @@ MediaRecorder.prototype = {
    * })
    */
   start: function start (timeslice) {
+    /**
+      Create a clone of stream and start recording
+    */
     if (this.state !== 'inactive') {
       return this.em.dispatchEvent(error('start'))
     }
@@ -87,7 +90,8 @@ MediaRecorder.prototype = {
     if (!context) {
       context = new AudioContext()
     }
-    var input = context.createMediaStreamSource(this.stream)
+    this.clonedStream = this.stream.clone()
+    var input = context.createMediaStreamSource(this.clonedStream)
     var processor = context.createScriptProcessor(2048, 1, 1)
 
     var recorder = this
@@ -124,13 +128,16 @@ MediaRecorder.prototype = {
    * })
    */
   stop: function stop () {
+    /**
+      Stop stream and end cloned stream tracks
+    */
     if (this.state === 'inactive') {
       return this.em.dispatchEvent(error('stop'))
     }
 
     this.requestData()
     this.state = 'inactive'
-    this.stream.getTracks().forEach(track => track.stop())
+    this.clonedStream.getTracks().forEach(track => track.stop())
     return clearInterval(this.slicing)
   },
 
